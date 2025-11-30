20eb01aadf2e2a8cb1f7b9d09be5ad5bfc3d03d0
Gouri Jat <gourijat26@gmail.com>
2025-11-30 19:58:49 +0530

    updating server.js

--- server.js
+++ server.js
@@ -2,9 +2,21 @@
 const express = require("express");
 const app = express();
 
+let users = [
+    {id:1,name: "mannat"},
+    {id:2, name : "xyz" }
+];
+
 app.get("/",(req,res) =>{
     res.send("Now creating server");
 });
+ app.get("/users",(req,res) =>{
+    res.send("user CRUD creation");
+ });
+
+//lest try CRUD in express manually
+
+
 
 app.listen(3000,() =>{
     console.log("Server is starting Yahooo");
